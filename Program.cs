9ce097b012d211b7598659cc4468d784b82483f3
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-04-09 09:10:43 -0300

    Cria o DbContext do projeto

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ builder.Services.AddSwaggerGen();
 
 // Comfigura o do Entity Framework Core com MySQL
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-// builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString,
-//     new MySqlServerVersion(new Version(8, 0, 41))));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString,
+    new MySqlServerVersion(new Version(8, 0, 41))));
 
 var app = builder.Build();
 
---
+++ src/context/AppDbContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleNotas.src.context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>()
+                .HasKey(a => a.Id);
+        }
+    }
+}
