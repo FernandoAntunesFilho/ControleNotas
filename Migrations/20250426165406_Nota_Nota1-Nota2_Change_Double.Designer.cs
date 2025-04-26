7dd6b6ff8c33290cb1353060dbe213dba5b4c625
Fernando Antunes Filho <fernando.antunes1@gmail.com>
2025-04-26 15:27:46 -0300

    Cria migrations para alterar os campos notas da Tabela Nota

--- Migrations/20250426165406_Nota_Nota1-Nota2_Change_Double.Designer.cs
+++ Migrations/20250426165406_Nota_Nota1-Nota2_Change_Double.Designer.cs
@@ -4,6 +4,7 @@ using ControleNotas.src.context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ControleNotas.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250426165406_Nota_Nota1-Nota2_Change_Double")]
+    partial class Nota_Nota1Nota2_Change_Double
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
